824927c7e3e6e964a9909b086839a654ef60a05d
Taimoor Ansari <ma07595@st.habib.edu.pk>
2022-10-12 22:11:03 +0500

    hu mania comments and finalized

--- HW3/HUMania/HUMania.hpp
+++ HW3/HUMania/HUMania.hpp
@@ -8,28 +8,18 @@ struct Unit
     SDL_Rect srcRect, moverRect;
 };
 
+// ANIMAL CLASS DEFINITION
 class Animal
 {
 public:
     Unit animals;
-    int condition = 0;
-    // Animal(Unit anm = NULL, int cond = 0)
-    // {
-    //     condition = cond;
-    //     animals = anm;
-    // };
+    int state = 0;
 };
-
+//ANIMATION CLASS TO CHANGE STATE OF EACH ANIMAL
 class Animation
 {
 public:
     Unit animation1;
     Unit animation2;
     Unit animation3;
-    // Animation(Unit a1 = NULL, Unit a2 = NULL, Unit a3 = NULL)
-    // {
-    //     animation1 = a1;
-    //     animation2 = a2;
-    //     animation3 = a3;
-    // };
 };
